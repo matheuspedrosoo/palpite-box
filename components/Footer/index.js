1967ba909321600f9bc440eb53a7ae0356bb3629
matheussbs <76970330+matheussbs@users.noreply.github.com>
2021-01-12 17:53:45 -0300

    atualização link e README

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className='bg-gray-700 p-4'>
       <div className='container max-auto text-center font-bold text-white'>
         Projeto desenvolvido por: Matheus Pedroso / {' '}
-        <a className='hover:underline' href='https://www.linkedin.com/in/matheupedroso/'>Linkedin</a> / {' '}
+        <a className='hover:underline' href='https://www.linkedin.com/in/matheuspedrosoo/'>Linkedin</a> / {' '}
         <a className='hover:underline' href='https://github.com/matheuspedrosoo'>Github</a>
         <div className='mt-4'>
           <img className='inline p-4 ' height="50" width="200" src='logo_semana_fsm.png' />
